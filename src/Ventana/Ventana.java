17a690f1fa5bcc924a4fad25d2d94944326d3138
Raziel <razzziel001@gmail.com>
2019-03-24 20:01:36 -0500

    Creada la ventana mirando la API JFrame

---
+++ src/Ventana/Ventana.java
@@ -0,0 +1,16 @@
+
+package Ventana;
+
+import javax.swing.JFrame; //Para diseñar cuadros de dialogo, creo
+
+
+public class Ventana extends JFrame{
+    public Ventana(){ //Constructor //Se revisa la api para ver bien como funcionan los metodos
+        this.setSize(500, 500);//Esto establece el tamaño de la ventana
+        
+        
+
+        
+    }
+    
+}
