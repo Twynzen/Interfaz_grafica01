7be6253073120844885ffa5b8306d8570a07c77d
Raziel <razzziel001@gmail.com>
2019-03-26 15:03:06 -0500

    Se aplicaron metodos de ubicación de aparicion de ventana

--- src/Ventana/Ventana.java
+++ src/Ventana/Ventana.java
@@ -6,11 +6,15 @@ import javax.swing.JFrame; //Para diseñar cuadros de dialogo, creo
 
 public class Ventana extends JFrame{
     public Ventana(){ //Constructor //Se revisa la api para ver bien como funcionan los metodos
-        this.setSize(500, 500);//Esto establece el tamaño de la ventana
+       setBounds(700,300,500,500);//Establece la posicion de apareicion y el tamaño de la ventana 
+       
+        //this.setSize(500, 500);//Esto establece el tamaño de la ventana
+         setTitle("Titulo de ventana"); // Establrece e titulo de la ventana
+         setDefaultCloseOperation(EXIT_ON_CLOSE); //Metodo para cerrar la ventana y que se termine el programa
+       
+        //setLocation(700,300);//por medio de plano cartesiano ubica la aparición de la ventana
         
-        setDefaultCloseOperation(EXIT_ON_CLOSE); //Metodo para cerrar la ventana y que se termine el programa
-        setTitle("Titulo de ventana"); // Establrece e titulo de la ventana
-        
+       
         
         
 
