91953a9fbf49a76cb5daa6c216f92fdd798c6307
Raziel <razzziel001@gmail.com>
2019-03-26 15:08:38 -0500

    Incluyo un metodo que por defecto ubique la aparicion de la ventana

--- src/Ventana/Ventana.java
+++ src/Ventana/Ventana.java
@@ -6,15 +6,14 @@ import javax.swing.JFrame; //Para diseñar cuadros de dialogo, creo
 
 public class Ventana extends JFrame{
     public Ventana(){ //Constructor //Se revisa la api para ver bien como funcionan los metodos
-       setBounds(700,300,500,500);//Establece la posicion de apareicion y el tamaño de la ventana 
-       
-        //this.setSize(500, 500);//Esto establece el tamaño de la ventana
+        this.setSize(500, 500);//Esto establece el tamaño de la ventana
          setTitle("Titulo de ventana"); // Establrece e titulo de la ventana
          setDefaultCloseOperation(EXIT_ON_CLOSE); //Metodo para cerrar la ventana y que se termine el programa
-       
+         setLocationRelativeTo(null);//Al usar este por defecto ejecuta la ventana en el centro
+         
         //setLocation(700,300);//por medio de plano cartesiano ubica la aparición de la ventana
         
-       
+        //setBounds(700,300,500,500);//Establece la posicion de apareicion y el tamaño de la ventana
         
         
 
